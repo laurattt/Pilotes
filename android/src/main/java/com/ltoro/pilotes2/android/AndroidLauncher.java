16a33855a1496ebf05233cc5d4109a7dd12d73ab
laurattt <lauratoro422@gmail.com>
2026-03-09 16:20:18 +0100

    Initial commit

---
+++ android/src/main/java/com/ltoro/pilotes2/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.ltoro.pilotes2.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ltoro.pilotes2.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
---
+++ core/src/main/java/com/ltoro/pilotes2/Main.java
@@ -0,0 +1,67 @@
+package com.ltoro.pilotes2;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    public FitViewport viewport;
+    Texture pilotaTexture;
+
+    float posx, posy, velx, vely;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        viewport = new FitViewport(8, 5); // 8x5 unitats (amplada x alçada)
+
+        // Creem la Texture de la pilota (format en pixels! , de moment)
+        Pixmap pilotaPixmap = new Pixmap(100,100,Pixmap.Format.RGBA8888);
+        pilotaPixmap.setColor(Color.RED);
+        pilotaPixmap.fillCircle(50,50,50); // posx, posy, radi
+        pilotaTexture = new Texture(pilotaPixmap);
+
+        // Moviment
+        posx = posy = 0f;
+        velx = vely = 1f;
+    }
+
+
+    @Override
+    public void render() {
+        // pintem background de color
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        // PUNT CLAU: ajustem càmera per traduir resolucions quan pintem
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        // Calculem
+        float delta = Gdx.graphics.getDeltaTime();
+        posx = posx + velx * delta;
+
+        // pintem!
+        batch.begin();
+        // ...instruccions de dibuix amb "coordenades virtuals"...
+        //batch.draw(pilotaTexture,2f,2f,1f,1f);
+        batch.draw(pilotaTexture,posx,posy,1f,1f);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
